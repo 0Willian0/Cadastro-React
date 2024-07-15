7f239dd3ea2f175de9c32c255e3be8035f87763e
0WiIIian0 <84929084+0Willian0@users.noreply.github.com>
2024-07-15 15:45:48 -0300

    front end

---
+++ src/components/template/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className='footer'>
+        <span>
+            Desenvolvido com muito <i className='fa fa-heart text-danger'></i>
+            <br />
+            <strong>At.</strong> <span className='text-danger'>Eu</span>
+
+        </span>
+    </footer>
